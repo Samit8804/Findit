208715900ea9a2f18d22c4bad673663f753bf801
samit fartyal <samitfartyal@gmail.com>
2026-09-13 18:12:35 +0530

    Update Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -28,7 +28,7 @@ export const Button: React.FC<ButtonProps> = ({
   const sizes = {
     sm: 'px-3 py-1.5 text-xs',
     md: 'px-4 py-2 text-sm',
-    lg: 'px-6. py-3 text-base',
+    lg: 'px-6 py-3 text-base',
   };
 
   return (
